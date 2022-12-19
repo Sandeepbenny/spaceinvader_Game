412bad33ddaf1ab7c853bec4e8869b4533fe667c
Sandeepbenny <sandeep@sanduchackonal@gmail.com>
2022-12-19 18:08:45 +0530

    Done with bullet

--- main.py
+++ main.py
@@ -32,9 +32,10 @@ enemyY_change = 40
 
 bulletimg = pygame.image.load('bullet.png')
 bulletX = 0
-bulletY = random.randint(50, 150)
+bulletY = 480
 bulletX_change = 0
-bulletY_change = 40
+bulletY_change = 10
+bullet_state = "ready"
 
 
 def player(x, y):
@@ -43,6 +44,12 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyimg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletimg, (x + 16, y + 10))
 
 
 # game loop
@@ -62,16 +69,19 @@ while running:
                 playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
-# player boundary
+    # player boundary
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
-# enemy boundary
+    # enemy boundary
     enemyX += enemyX_change
     if enemyX <= 0:
         enemyX_change = 0.3
@@ -80,6 +90,13 @@ while running:
         enemyX_change = -0.3
         enemyY += enemyY_change
 
+# bullet movement
+    if bullet_state is "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY -= bulletY_change
+
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
+
